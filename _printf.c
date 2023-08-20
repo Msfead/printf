569d3fb22330bebe8fc689144c240889410f3410
Msfead <Msfead2015@gmail.com>
2023-08-20 06:43:42 -0700

    M

---
+++ _printf.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - printf fun
+ * @format: format
+ * by mostafa and soha
+ * Return: printed char
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/*write(1, &format[i], 1)*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			sizee = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer
+			printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+					if (printed == -1)
+					return (-1);
+					printed_chars += printed;
+					}
+					}
+					print_buffer(buffer, &buff_ind);
+
+					va_end(list);
+
+					return (printed_chars);
+					}
+
+					/**
+					 * print_buffer - print contents
+					 * @buffer: array of char
+					 * @buff_ind: index at next char
+					 */
+void print_buffer(char buffer[], int *buff_ind);
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buf_ind = 0;
+}
