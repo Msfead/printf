2eee0154a226870174581a8c2c71dc7a07347231
Msfead <Msfead2015@gmail.com>
2023-08-20 18:19:34 -0700

    M

--- _printf.c
+++ _printf.c
@@ -36,7 +36,7 @@ int _printf(const char *format, ...)
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
-			sizee = get_size(format, &i);
+			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer
 			printed = handle_print(format, &i, list, buffer,
